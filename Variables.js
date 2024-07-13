e3b5d6e4d569a3e8785dc59d2f8ffb602085430f
MindMentorPro <parakhiyavidhi@gmail.com>
2024-07-13 17:30:51 -0400

    updated file

--- Variables.js
+++ Variables.js
@@ -1,32 +1,36 @@
-// Activity1
-var num=123;
+// VIDHI RAMESH PARAKHIYA
+// 30 DAYS CHALLENGE
+
+// Activity1 Variable Declaration
+var numbers=123;
 let name="vidhiRameshParakhiya"
+console.log(numbers,name)
 
-// Activity2
-const values=Boolean=5>3;
-console.table([num,name,values]);
+// Activity2 Constant declaration
+const boolean=Boolean=5>3;
+console.table([numbers,name,boolean]);
 
-// Activity3
+// Activity3 Data types
 var Numbers=9865623562;
 let letters="itsmyname";
-let newvalues=Boolean==6<3;
+let bool=Boolean==6<3;
 let object={name:"vidhi",bestfriend:"deepali"}
-let a=[1,2,3]
-console.log(typeof a,Numbers,letters,newvalues,object);
+let array=[1,2,3]
+console.log(typeof array,Numbers,letters,bool,object);
 
 // Activity4
 let year=856;
 year=988;
-console.log('year=',year)
+console.log('let year is=',year)
 
 // Activity5
 const years=123;
 // v=666; cannot reassign const value
-console.log('years=',years);
+console.log('const years is=',years);
 
 // NOTES
 // We can change value of let from memory but
 // we cannot change value of const,once its assign it cannot change
 // Example-Before in js we used to use var but if one developers change one name 
-// vale than whrever there is name keyword everthing gets ChannelMergerNode.so we use const and let more 
+// value than wherever there is name keyword, everthing gets Change.so we use const and let more 
 // avoid using var.
