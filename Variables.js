81dd928123ea824cc1ad431d8e8313f46c431c62
MindMentorPro <parakhiyavidhi@gmail.com>
2024-07-13 16:52:33 -0400

    30 days challenge

---
+++ Variables.js
@@ -0,0 +1,32 @@
+// Activity1
+var num=123;
+let name="vidhiRameshParakhiya"
+
+// Activity2
+const values=Boolean=5>3;
+console.table([num,name,values]);
+
+// Activity3
+var Numbers=9865623562;
+let letters="itsmyname";
+let newvalues=Boolean==6<3;
+let object={name:"vidhi",bestfriend:"deepali"}
+let a=[1,2,3]
+console.log(typeof a,Numbers,letters,newvalues,object);
+
+// Activity4
+let year=856;
+year=988;
+console.log('year=',year)
+
+// Activity5
+const years=123;
+// v=666; cannot reassign const value
+console.log('years=',years);
+
+// NOTES
+// We can change value of let from memory but
+// we cannot change value of const,once its assign it cannot change
+// Example-Before in js we used to use var but if one developers change one name 
+// vale than whrever there is name keyword everthing gets ChannelMergerNode.so we use const and let more 
+// avoid using var.
